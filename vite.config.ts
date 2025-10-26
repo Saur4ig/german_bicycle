31d57a48ee57d4468d955bf98776db32d8b651ae
saur4ig <saurons909@gmail.com>
2025-10-26 20:59:49 +0100

    fixed vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bike-parts-game/",
+  base: "/german_bicycle/",
 });
